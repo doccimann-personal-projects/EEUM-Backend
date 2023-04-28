8030f96c0eb9643c524d3c7a041c3823e72393ae
BrianDYKim <18k7102dy@naver.com>
2023-04-28 17:31:11 +0900

    add: Login 관련 swagger 문서 작성 완료

--- src/users/presentation/controller/users.controller.ts
+++ src/users/presentation/controller/users.controller.ts
@@ -6,6 +6,7 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserResponse } from '../../application/dto/response/create-user.response';
 import { FailureResult } from '../../../common/response/failure-response.format';
 import { LoginUserRequest } from '../../application/dto/request/login-user.request';
+import { LoginUserResponse } from '../../application/dto/response/login-user.response';
 
 @ApiTags('인증/인가')
 @Controller('api/users')
@@ -36,6 +37,24 @@ export class UsersController {
     return await this.usersService.signup(createRequest);
   }
 
+  @ApiOperation({
+    summary: '일반 사용자 로그인입니다',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '로그인 성공 응답입니다',
+    type: LoginUserResponse,
+  })
+  @ApiResponse({
+    status: 401,
+    description: '로그인 실패 응답입니다',
+    type: FailureResult,
+  })
+  @ApiResponse({
+    status: 500,
+    description: '내부 서버 에러입니다',
+    type: FailureResult,
+  })
   @Post('/login')
   async jwtLogin(@Body() loginRequest: LoginUserRequest) {
     return await this.usersService.login(loginRequest);
