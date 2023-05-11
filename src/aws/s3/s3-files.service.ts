5b29cc84f083556367a8ff9ba0aad68cd4180513
BrianDYKim <18k7102dy@naver.com>
2023-05-11 19:02:23 +0900

    fix: getSignedUrl을 콜백 기반에서 프로미스 기반으로 변경

--- src/aws/s3/s3-files.service.ts
+++ src/aws/s3/s3-files.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 import * as AWS from 'aws-sdk';
 import { v4 } from 'uuid';
 import { extname } from 'path';
+import { promisify } from 'util';
 
 @Injectable()
 export class S3FilesService {
   private readonly s3: AWS.S3 = new AWS.S3();
   private readonly bucketName: string = process.env.BUCKET_NAME;
+
+  // getSignedUrl을 promise 기반의 함수로 변경
+  getSignedUrlAsync = promisify(this.s3.getSignedUrl).bind(this.s3);
 
   // 1개의 파일만을 업로드하는 메소드
   async uploadFile(
@@ -28,11 +32,9 @@ export class S3FilesService {
       .promise();
 
     // s3에 업로드 된 파일 url을 가져온다
-    const fileUrl: string = this.s3.getSignedUrl('getObject', {
+    return await this.getSignedUrlAsync('getObject', {
       Bucket: this.bucketName,
       Key: fileKey,
     });
-
-    return fileUrl;
   }
 }
